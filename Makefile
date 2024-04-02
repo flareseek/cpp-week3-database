9650d3f35a7df3bb3bc14562c16887a1cc33d9df
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-04-02 17:12:25 +0900

    [Fix] separate code

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-main: main.o
-	g++ -o ./main ./main.cpp
+main: main.o database.o
+	g++ -o ./main ./*.o
 
-main.o : main.cpp database.h
-	g++ -Werror -std=c++11 -c ./main.cpp
+main.o : main.cpp database.cpp database.h
+	g++ -Werror -std=c++11 -c ./*.cpp
--- database.h
+++ database.h
@@ -3,6 +3,12 @@
 
 #include <string>
 
+#define INT_STRING "int"
+#define DOUBLE_STRING "double"
+#define STRING_STRING "string"
+#define ARRAY_STRING "array"
+#define EXIT_STRING "exit"
+
 enum Type { INT, DOUBLE, STRING, ARRAY };
 
 struct Array {
@@ -41,4 +47,34 @@ void remove(Database &database, std::string &key);
 // 데이터베이스를 해제한다.
 void destroy(Database &database);
 
+// ---------------------------------------------
+// key값을 받아옵니다.
+std::string getKey();
+
+// type값을 받아옵니다.
+Type getType();
+
+// 타입에 맞게 데이터를 받고 그 주소를 반환합니다.
+void* getTypeValueAddress(Type type);
+
+// 배열을 삭제합니다.
+void deleteArray(Array *array);
+
+// void 포인터를 해제합니다.
+void deleteVoidPointer(Type type, void* value);
+
+// entry를 해제합니다.
+void deleteEntry(Entry *entry);
+
+// 타입에 맞게 값을 출력합니다.
+void printValue(Type type, void* value);
+
+// 배열을 출력합니다.
+void arrayPrint(Array* array);
+
+// 배열을 생성합니다.
+Array* createArray();
+
+// entry를 출력합니다.
+void printEntry(Entry* entry);
 #endif
